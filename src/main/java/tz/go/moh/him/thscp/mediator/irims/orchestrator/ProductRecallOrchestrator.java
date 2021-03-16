9a0620b7890c80907a6fba1d1ac4ca03c17b87be
Danford Nchembi <danfordnchembi@gmail.com>
2021-03-16 11:22:23 +0300

    Reducing the complexity of validate data function

--- src/main/java/tz/go/moh/him/thscp/mediator/irims/orchestrator/ProductRecallOrchestrator.java
+++ src/main/java/tz/go/moh/him/thscp/mediator/irims/orchestrator/ProductRecallOrchestrator.java
@@ -94,7 +94,6 @@ public class ProductRecallOrchestrator extends UntypedActor{
                 resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("ERROR_INVALID_PAYLOAD"), null));
             } else {
                 resultDetailsList.addAll(validateRequiredFields(irimsRequest));
-                resultDetailsList.addAll(validateDateRequiredFields(irimsRequest));
                 resultDetailsList.addAll(validateDateFields(irimsRequest));
             }
 
@@ -133,6 +132,9 @@ public class ProductRecallOrchestrator extends UntypedActor{
         if (StringUtils.isBlank(irimsRequest.getBatchNumber()))
             resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("BATCH_NUMBER_IS_BLANK"), null));
 
+        if (StringUtils.isBlank(irimsRequest.getClosureDate()))
+            resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("CLOSURE_DATE_IS_BLANK"), null));
+
         if (StringUtils.isBlank(irimsRequest.getDescription()))
             resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("DESCRIPTION_IS_BLANK"), null));
 
@@ -142,41 +144,21 @@ public class ProductRecallOrchestrator extends UntypedActor{
         if (StringUtils.isBlank(irimsRequest.getIssue()))
             resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("ISSUE_IS_BLANK"), null));
 
+        if (StringUtils.isBlank(irimsRequest.getRecallDate()))
+            resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("RECALL_DATE_IS_BLANK"), null));
+
         if (StringUtils.isBlank(String.valueOf(irimsRequest.getRecallFrequency())))
             resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("RECALL_FREQUENCY_IS_BLANK"), null));
 
         if (StringUtils.isBlank(String.valueOf(irimsRequest.getRecalledQuantity())))
             resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("RECALLED_QUANTITY_IS_BLANK"), null));
 
-        if (StringUtils.isBlank(irimsRequest.getUnit()))
-            resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("UNIT_IS_BLANK"), null));
-
-        return resultDetailsList;
-    }
-
-    public List<ResultDetail> validateDateRequiredFields(IRIMSRequest irimsRequest) {
-        List<ResultDetail> resultDetailsList = new ArrayList<>();
-
-        if (StringUtils.isBlank(irimsRequest.getClosureDate()))
-            resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("CLOSURE_DATE_IS_BLANK"), null));
-
-        if (StringUtils.isBlank(irimsRequest.getRecallDate()))
-            resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("RECALL_DATE_IS_BLANK"), null));
-
         if (StringUtils.isBlank(String.valueOf(irimsRequest.getStartDate())))
             resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("START_DATE_IS_BLANK"), null));
 
-        return resultDetailsList;
-    }
+        if (StringUtils.isBlank(irimsRequest.getUnit()))
+            resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("UNIT_IS_BLANK"), null));
 
-    /**
-     * Validate iRIMS Request Date Fields
-     *
-     * @param irimsRequest to be validated
-     * @return array list of validation results details for failed date validations
-     */
-    public List<ResultDetail> validateDateFields(IRIMSRequest irimsRequest) {
-        List<ResultDetail> resultDetailsList = new ArrayList<>();
         try {
             if (!DateValidatorUtils.isValidPastDate(irimsRequest.getRecallDate(), checkDateFormatStrings(irimsRequest.getRecallDate()))) {
                 resultDetailsList.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("ERROR_RECALL_DATE_IS_NOT_VALID_PAST_DATE"), null));
